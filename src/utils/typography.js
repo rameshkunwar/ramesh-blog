47e2004e2caeff16dca5163727ad13c6a07be898
Ramesh Kunwar <rameshkunwar@hotmail.com>
2020-09-20 01:04:53 +0200

    p style overridden in typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,6 +5,13 @@ fairyGateTheme.baseFontSize = "16px" //"22px"
 fairyGateTheme.baseLineHeight = "1.4" //"1.64"
 fairyGateTheme.bodyColor = "#0a0a23"
 
+fairyGateTheme.overrideStyles = options => ({
+  p: {
+    letterSpacing: 0.2,
+    color: "rgb(5, 6, 7)",
+    fontSize: "1.5rem",
+  },
+})
 const typography = new Typography(fairyGateTheme)
 
 export const { scale, rhythm, options } = typography
