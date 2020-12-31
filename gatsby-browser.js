53083f14b33e0f160a8ca0042d05adcd71f97e8c
Ramesh Kunwar <rameshkunwar@hotmail.com>
2020-12-31 19:51:25 +0100

    prompt added for new content in gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
 // trigger an immediate page refresh when an update is found
-export const onServiceWorkerUpdateReady = () => window.location.reload();
+exports.onServiceWorkerUpdateReady = () => {
+    if (
+      window.confirm(
+        "This site has been updated with new data. Do you wish to reload the site to get the new data?"
+      )
+    ) {
+      window.location.reload(true);
+    }
+  };
