fc82a09b99c704e98e5b63c2d79661706307b0da
Ramesh Kunwar <rameshkunwar@hotmail.com>
2021-05-17 22:14:40 +0200

    duplicate images deleted, new blog added

---
+++ content/posts/012-asp-react-typescript-template/index.md
@@ -0,0 +1,14 @@
+---
+title: "A template for asp.net mvc/web api with React js with Typescript & webpack"
+created: "2021-05-16T20:03:05.632"
+modified: "2021-05-16T20:03:05.632"
+description: "Use React JS with Typescript as a view with asp.net mvc/web api (.net framework 4.xx). Inject scripts & styles on _Layout.cshtml on build"
+featuredImage: "../../images/blog/blog-012-asp-net-mvc-react-typescript-webpack-img001.jpg"
+imageCaption: ""
+slug: asp-dot-net-mvc-react-js-typescript-webpack-template
+tags: ["asp.net mvc", "asp.net web api", "react js", "typescript"]
+type: post
+blogMonth: May 2020
+---
+
+![blog image](../../images/blog/blog-012-asp-net-mvc-react-typescript-webpack-img001.jpg " ")
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,15 @@
 // trigger an immediate page refresh when an update is found
 exports.onServiceWorkerUpdateReady = () => {
-    if (
-      window.confirm(
-        "This site has been updated with new data. Do you wish to reload the site to get the new data?"
-      )
-    ) {
-      window.location.reload(true);
-    }
-  };
+  if (
+    window.confirm(
+      "This site has been updated with new data. Do you wish to reload the site to get the new data?"
+    )
+  ) {
+    // window.location.reload(true); //deprecated
+    const form = document.createElement("form")
+    form.method = "POST"
+    form.action = window.location.href
+    document.body.appendChild(form)
+    form.submit()
+  }
+}
